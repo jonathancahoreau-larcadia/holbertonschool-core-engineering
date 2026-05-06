eaf63330b16d2d57d8d5e2cd51dfcf1b2207b1cc
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-07 01:17:21 +0200

    add variable

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -2,6 +2,8 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+    a = 10
+    b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)), end="")
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)), end="")
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)), end="")
