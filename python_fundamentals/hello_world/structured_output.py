8ed2cf53fbe262f3514d4bc5c6dc46412ce62279
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-06 08:59:24 +0200

    send file to github

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+pi_approx = 3.1415
+is_valid = (10 > 8)
+print(f"Language: Python")
+print(f"Version: 3")
+print(f"Pi approx: {pi_approx: .2f}")
+print(f"Computation valid: {is_valid}")
