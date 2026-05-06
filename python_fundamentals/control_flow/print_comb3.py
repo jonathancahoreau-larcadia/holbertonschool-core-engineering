ba77bc56f56bac77309f5edaffedd13846ad1e2f
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-06 10:07:33 +0200

    change print

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -2,6 +2,6 @@
 for dizaine in range(0, 10):
     for unite in range(dizaine + 1, 10):
         if dizaine == 8 and unite == 9:
-            print(f"{dizaine}{unite}", end='\n')
-            break
-        print(f"{dizaine}{unite}", end=", ")
+            print("{}{}".format(dizaine, unite), end='\n')
+        else:
+            print("{}{}".format(dizaine, unite), end=", ")
