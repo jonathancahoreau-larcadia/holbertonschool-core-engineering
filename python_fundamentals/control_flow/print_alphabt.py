ca0f033e71bec7ed6457e23aaea87be4fe5a0a04
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-06 09:43:30 +0200

    just to see

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/env python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    if not chr(alphabet) == 'e' and not chr(alphabet) == 'q':
-        print(chr(alphabet), end="")
-print()
+print("abcdfghijklmnoprstuvwxyz")
