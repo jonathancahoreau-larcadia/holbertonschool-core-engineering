c532b596e46a173f9d667c9ebac4143cddfb13b8
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-06 09:47:27 +0200

    new code

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-print("abcdfghijklmnoprstuvwxyz")
+for code in range(ord('a'), ord('z') + 1):
+    ch = chr(code)
+    if ch in ('e', 'q'):
+        continue
+    print(ch, end="")
+print()
