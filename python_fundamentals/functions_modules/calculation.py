5e46b4e9ce19c4cfaf764e6afb7bd7b7769fde2d
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-07 01:13:46 +0200

    send to github

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
