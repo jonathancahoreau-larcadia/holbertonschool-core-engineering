6f5e3443926f6a1edc3605635b4d9923ed383ce7
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-06 09:21:58 +0200

    delete space before .2f

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -3,5 +3,5 @@ pi_approx = 3.1415
 is_valid = (10 > 8)
 print(f"Language: Python")
 print(f"Version: 3")
-print(f"Pi approx: {pi_approx: .2f}")
+print(f"Pi approx: {pi_approx:.2f}")
 print(f"Computation valid: {is_valid}")
