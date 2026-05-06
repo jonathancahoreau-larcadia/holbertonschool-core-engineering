692c6376e5cd57a5a10fad33eae6b18074104fe1
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-06 15:15:31 +0200

    without .join

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    print("".join(chr(ord(c) - 32) if ord('a')<= ord(c) <= ord('z') else c for c in str))
-    
+    print(*[chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in str], sep="")
