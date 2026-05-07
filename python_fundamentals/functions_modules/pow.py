83aaa095d9338eaade703688adb937f068a58200
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-07 11:30:59 +0200

    add b<0

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,7 +2,15 @@
 def pow(a, b):
     count = 0
     result = 1
-    while (count < b):
-        count += 1
-        result = result * a
-    return result
+    if (b < 0):
+        b = -b
+        while (count < b):
+            count += 1
+            result = result * a
+        result = 1/result
+        return result
+    else:
+        while (count < b):
+            count += 1
+            result = result * a
+        return result
