dd99698b74f0cbc965ca67004d465d9bda0e30df
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-11 19:34:55 +0200

    send to github

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for row in range(self.__size):
+                    print("{}".format("#"), end="")
+                print()
