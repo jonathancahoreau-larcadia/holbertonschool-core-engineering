2ab24fc086cdf985e236839f4b60f4aaedda6d6d
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-13 14:59:57 +0200

    faute de frappe

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Définit la largeur du rectangle."""
-        if isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -33,8 +33,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Définit la hauteur du rectangle."""
-        if isinstance(value, int):
-            raise TypeError("width must be an integer")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -38,7 +38,7 @@ class Square:
         if type(value) is not tuple or len(value) != 2 or type(
             value[0]) is not int or type(value[1]) is not int or value[
                 0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
