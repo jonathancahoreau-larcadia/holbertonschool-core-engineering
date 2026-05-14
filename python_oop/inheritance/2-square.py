e2b65b6618384023afe095f9a6b0a794d4eb7842
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-14 19:31:12 +0200

    send to github

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module defines the Square class, inheriting from Rectangle.
+"""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    Represents a square with equal sides.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
